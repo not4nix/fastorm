f79520ac37cf23212a2cde68fcb43f5248747b19
Viacheslav Okolitiy <justforgit@gmail.com>
2013-02-17 21:42:03 +0200

    Minor changes

--- src/com/fastorm/query/BaseQuery.java
+++ src/com/fastorm/query/BaseQuery.java
@@ -1,4 +1,17 @@
 package com.fastorm.query;
 
-public abstract class BaseQuery {
+public abstract class BaseQuery implements Queryable{
+
+    public BaseQuery(){
+    }
+
+    /**
+     * This method must be overridden in child class
+     * @param query
+     * @return query string
+     */
+    @Override
+    public String buildQuery(StringBuilder query) {
+        return query.toString();
+    }
 }
